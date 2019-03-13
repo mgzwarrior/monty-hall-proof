b319ce68383753926d67c6e3209739b6d89495a2
Matthew Grant <mgzwarrior@Matthews-MacBook-Air.local>
2019-03-13 00:03:03 -0400

    - Implemented script - Updated README

--- README.md
+++ README.md
@@ -2,5 +2,30 @@
 ## Author: Matt Grant
 A program to prove the Monty Hall problem.
 
-# Input Arguments
+# Background
+The [Monty Hall problem](https://en.wikipedia.org/wiki/Monty_Hall_problem "Monty Hall problem Wikipedia") is a brain teaser, in the form of a probability puzzle, loosely based on the American television game show *Let's Make a Deal* and named after its original host, *Monty Hall*.
+
+## Problem Question
+> Suppose you're on a game show, and you're given the choice of three doors: Behind one door is a car; behind the others, goats. You pick a door, say No. 1, and the host, who knows what's behind the doors, opens another door, say No. 3, which has a goat. He then says to you, "Do you want to pick door No. 2?" Is it to your advantage to switch your choice?
+
+## Assertion
+In theory, contestants who switch to the remaining unopened door will win the car two out of every three games.  On the other hand, contestants who choose to stay with their original choice will win the car only one out of every three games.  This program aims to prove this assertion by simulating both scenatios a given number of times to determine if it is truly in the contestant's best interest to switch their choice.
+
+# Running Instructions
+This section outlines how to run the script.
+
+## Input Arguments
+1. Number of games for each scenario (eg. 100, 1000, etc.)
+2. Enable/disable logging (eg. True or False)
+
+### Example
+`python3 monty-hall-proof.py 100000 True`
+
+# Results
 TBD
+
+# To-Do
++ Repalce 'empty' doors with goats
++ Review game scenarios for accuracy
++ Document results
++ Performance improvements
--- monty-hall-proof.py
+++ monty-hall-proof.py
@@ -1,29 +1,139 @@
+import sys
+import math
 import random
+
+if(len(sys.argv) < 3):
+	print('ERROR: Invalid arguments - see README for running instructions')
+	sys.exit()
+totalGames = int(sys.argv[1])
+suppressPrintStatements = bool(sys.argv[2])
 
 class Door:
 
-	def __init__(self,number,status,hasCar):
+	def __init__(self,number,status,hasCar,isChosen):
 		self.number = number
 		self.status = status
 		self.hasCar = hasCar
+		self.isChosen = isChosen
 
 	def open(self):
 		if(self.status == 'open'):
-			print('Door ' + str(self.number) + ' is already ' + self.status)
+			if(not suppressPrintStatements):
+				print('ERROR: Door ' + str(self.number) + ' is already ' + self.status)
+				sys.exit()
 		else:
 			self.status = 'open'
 
-	def close(self):
-		if(self.status == 'closed'):
-			print('Door ' + str(self.number) + ' is already ' + self.status)
+			if(self.hasCar):
+				if(not suppressPrintStatements):
+					print('Winner!  Door ' + str(self.number) + ' has a car behind it')
+			else:
+				if(not suppressPrintStatements):
+					print('Door ' + str(self.number) + ' is open and empty')
+
+	def choose(self):
+		if(self.isChosen):
+			print('ERROR: Door ' + str(self.number) + ' is already chosen')
+			sys.exit()
 		else:
-			self.status = 'closed'
+			self.isChosen = True
+
+			if(not suppressPrintStatements):
+					print('Door ' + str(self.number) + ' has been chosen')
+
+	def unchoose(self):
+		if(not self.isChosen):
+			print('ERROR: Door ' + str(self.number) + ' is not chosen already')
+			sys.exit()
+		else:
+			self.isChosen = True
 
 	def printDoor(self):
-		print('Door ' + str(self.number) + ' is ' + self.status)
+		if(not suppressPrintStatements):
+			print('Door ' + str(self.number) + ' is ' + self.status)
 
-door1 = Door(1,'closed',False)
-door2 = Door(2,'closed',False)
-door3 = Door(3,'closed',False)
+def setupDoors():
+	door1 = Door(1,'closed',False,False)
+	door2 = Door(2,'closed',False,False)
+	door3 = Door(3,'closed',False,False)
+	doors = [door1,door2,door3]
 
-print(random.randint(1,3))
+	ran = random.randint(1,3)
+
+	for door in doors:
+		if(door.number == ran):
+			door.hasCar = True
+
+		door.printDoor()
+
+	return doors
+
+def chooseDoor(doors):
+	ran = random.randint(1,3)
+
+	for door in doors:
+		if(door.number == ran):
+			door.choose()
+			break
+
+def revealLosingDoor(doors):
+	unchosenDoors = []
+	revealedDoor = None
+
+	for door in doors:
+		if(door.isChosen):
+			continue
+		unchosenDoors.append(door)
+
+	for door in unchosenDoors:
+		if(door.hasCar):
+			continue
+		
+		revealedDoor = door
+		revealedDoor.open()
+		unchosenDoors.remove(door)
+
+	return unchosenDoors.pop()
+
+def switchDoor(doors,remainingUnchosenDoor):
+	for door in doors:
+		if(door.isChosen):
+			door.unchoose()
+			break
+
+	remainingUnchosenDoor.choose()
+
+def isWinner(doors):
+	for door in doors:
+		if(door.isChosen and door.hasCar):
+			door.open()
+			return True
+		if(door.isChosen and not door.hasCar):
+			if(not suppressPrintStatements):
+				print('Sorry, Door ' + str(door.number) + ' is empty')
+			return False
+
+numWins = 0
+
+for x in range(totalGames):
+	suppressPrintStatements = True
+	doors = setupDoors()
+	chooseDoor(doors)
+	remainingUnchosenDoor = revealLosingDoor(doors)
+	switchDoor(doors,remainingUnchosenDoor)
+	if(isWinner(doors)):
+		numWins += 1
+
+print('Winning percentage when switching doors: ' + str(math.trunc((numWins / totalGames) * 100)) + '%')
+
+numWins = 0
+
+for x in range(totalGames):
+	suppressPrintStatements = True
+	doors = setupDoors()
+	chooseDoor(doors)
+	revealLosingDoor(doors)
+	if(isWinner(doors)):
+		numWins += 1
+
+print('Winning percentage when keeping original door: ' + str(math.trunc((numWins / totalGames) * 100)) + '%')
